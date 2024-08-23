2dccc803b5b334505fd24b9ad0d52a57bcc519a9
Eng. Mohamed Abdel Qader Qassem <93353158+Tgm2022@users.noreply.github.com>
2024-08-23 20:57:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// var heading= document.getElementsByClassNames('accordion-btn');
+console.log(1 + 1);
+console.log(2 + 4, "hello world");
+var fName = "mohamed";
+var age = 20;
+var gen = "male";
+var nation = "egypt";
+
+console.log(fName);
+console.log(age);
+console.log(gen);
+console.log(nation);
